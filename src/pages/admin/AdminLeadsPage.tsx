5a89a1f4a2b131a3ab476d107cbb935dfad92623
ardy1004 <salambumiproperty@gmail.com>
2025-12-07 20:36:09 +0700

    Remove react-helmet and clean up Pages Router

--- src/pages/admin/AdminLeadsPage.tsx
+++
@@ -1,9 +0,0 @@
-import { LeadManagement } from '@/components/admin/LeadManagement';
-
-export default function AdminLeadsPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <LeadManagement />
-    </div>
-  );
-}
